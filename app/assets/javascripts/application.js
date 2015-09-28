7807dec87b209e0bb5c722cbd56993757dc41702
n-mouse <murahoid@gmail.com>
2015-09-28 12:11:09 +0300

    js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,8 @@
 // ...
-//= require jquery_ujs
 //= require jquery
+//= require jquery_ujs
 //= require jquery.ui.datepicker
 //= require jquery.ui.position
 //= require jquery.cookie
 //= require general
 //= require ba-throttle-debounce
-//= require masonry/jquery.masonry
